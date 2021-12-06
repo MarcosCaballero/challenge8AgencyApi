12c28757a474046df6d3d778bc25a88c7634cd76
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2021-12-06 14:58:39 -0300

    entorno habilitado

--- config/config.js
+++ config/config.js
@@ -6,7 +6,6 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: "mysql",
   },
   test: {
